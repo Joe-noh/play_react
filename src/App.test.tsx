3ed3f8747c96a29cdca890a264471c1574450f76
Joe-noh <goflb.jh@gmail.com>
2019-05-19 16:05:58 +0900

    テスト書いてみようね

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Simulate } from 'react-dom/test-utils';
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let root: HTMLElement
+
+beforeEach(() => {
+  root = document.createElement('div')
+  ReactDOM.render(<App />, root)
+})
+
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(root)
+})
+
+it('can add a todo', () => {
+  const todo = 'Write some more test cases.'
+  const input = root.querySelector('input[type="text"]')
+  const form = root.querySelector('form')
+
+  expect(root.textContent).not.toContain(todo)
+
+  Simulate.change(input, { target: { value: todo } })
+  Simulate.submit(form)
+
+  expect(root.textContent).toContain(todo)
 });
+
+it('do nothing if todo title is empty', () => {
+  const input = root.querySelector('input[type="text"]')
+  const form = root.querySelector('form')
+
+  Simulate.change(input, { target: { value: '    ' } })
+  Simulate.submit(form)
+
+  expect(root.textContent).not.toContain('    ')
+})
